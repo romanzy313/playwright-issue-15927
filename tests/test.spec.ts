6138764a3bfb3f1fe90e93b2ddd3d088ddfca9a1
Roman <romanvolovoy@protonmail.com>
2022-07-29 02:57:45 +0400

    rename

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("assert on plain web-component", async ({ page }) => {
+test("fails to assert on plain web-component", async ({ page }) => {
   await page.goto("/");
 
   await expect(page.locator("plain-component")).toHaveClass("findme");
   await expect(page.locator("plain-component")).toHaveText(/Hello world/);
 });
 
-test("assert on div in web-component", async ({ page }) => {
+test("ok assert on div in web-component", async ({ page }) => {
   await page.goto("/");
 
   await expect(page.locator("div-component")).toHaveClass("findme");
